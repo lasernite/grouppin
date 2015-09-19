31a18a7ae94321d4df1d5ecc4e633cddc195153b
Laser Nite <lasernite@gmail.com>
2015-09-19 16:34:57 -0400

    added basic sign up/log in with devise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
-  def index
-  	# Default if no city found by geolocator - neccesary for testing/developing locally
-  	if request.location.city == '' or request.location.city == nil
-  		@city = 'MIT'
-  	else
-  		@city = request.location.city
-  	end
-  end
+	def index
+		# Default if no city found by geolocator - neccesary for testing/developing locally
+		if request.location.city == '' or request.location.city == nil
+			@city = 'MIT'
+		else
+			@city = request.location.city
+		end
+
+		# Devise current user from session
+		@user = current_user
+	end
 end
