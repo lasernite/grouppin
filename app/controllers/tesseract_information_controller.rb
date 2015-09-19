3d893585f59615d82e078ed3d6997ea0469b777c
mayuri <mayuri@mit.edu>
2015-09-19 23:44:35 +0200

    Added tesseract OCR stuff

--- app/controllers/tesseract_information_controller.rb
+++ app/controllers/tesseract_information_controller.rb
@@ -2,6 +2,6 @@ class TesseractInformationController < ApplicationController
   skip_before_filter :verify_authenticity_token 
   def index
   	e = Grouppin::Application::ENGINE
-  	@text = e.text_for('app/assets/images/test_image.jpg').strip
+  	@text = e.lines_for('app/assets/images/test_image.jpg')
   end
 end
